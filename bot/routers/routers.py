72bc9e0af6fed7433011adfbdbf2be4fef0e1b2b
Lonely77 <83435403+Lonelyyyy77@users.noreply.github.com>
2025-02-05 13:37:22 +0200

    Update routers.py

--- bot/routers/routers.py
+++ bot/routers/routers.py
@@ -2,17 +2,17 @@ from aiogram import Router
 
 
 async def start_router(dp) -> Router():
-    from ..handlers.command_start import router as command_start_router
+    from bot.handlers.command_start import router as command_start_router
 
     dp.include_router(command_start_router)
 
 async def user_routers(dp) -> Router():
-    from ..handlers.user.send_num import router as user_send_num_router
+    from bot.handlers.user.send_num import router as user_send_num_router
 
     dp.include_router(user_send_num_router)
 
 
 async def admin_routers(dp) -> Router():
-    from ..handlers.admin.admin_panel import router as admin_panel_router
+    from bot.handlers.admin.admin_panel import router as admin_panel_router
 
     dp.include_router(admin_panel_router)
