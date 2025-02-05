17505019795395b2c4153a1f7899017d18fbe87f
Lonely77 <83435403+Lonelyyyy77@users.noreply.github.com>
2025-02-05 13:32:59 +0200

    Update start_bot.py

--- bot/start_bot.py
+++ bot/start_bot.py
@@ -5,8 +5,8 @@ import os
 from aiogram import Bot, Dispatcher, Router
 import dotenv
 
-from bot.database.db import init_db
-from bot.routers.routers import start_router, user_routers, admin_routers
+from database.db import init_db
+from routers.routers import start_router, user_routers, admin_routers
 
 dotenv.load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
